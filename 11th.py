b9aa91c471e48030b6b88ed0480ccc280a16fbbb
Reeba <reebarasheedahmed@gmail.com>
2025-03-22 13:04:59 +0500

    Add files via upload

---
+++ 11th.py
@@ -0,0 +1,25 @@
+x=10
+print(x)
+x=5
+x+=3
+print(x)
+a=10
+a**=3
+print(a)
+x=10
+y=20
+print(x==y)
+print(y!=x)
+print(x>=y)
+print(x>=10 and y<=3)
+print (x>=10 or y<=7)
+print (not (x>=10 or y<=3))
+x=["Ali" , "Ahmad"]
+y=["Ali" , "Ahmad"]
+z=y
+print(y==x) # Assignment operator 
+print(y is z)
+print (z is x) #is have a identity operator 
+print(z!=y)    # not equal operator 
+print(z is not y)
+print ("Ahmad" in y)
