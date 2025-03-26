3a1502594d7e934d7aac9f5c91f09e2e18261f9c
Reeba <reebarasheedahmed@gmail.com>
2025-03-26 05:21:20 +0500

    Add files via upload

---
+++ 12th.py
@@ -0,0 +1,9 @@
+# f-string
+letter="Hey my name is {}, And I'm from {} "
+country="Pakistan"
+name="Reeba"
+print(f"Hey my name is {name}, And I'm from {country}")
+price=50.0675
+txt=f"For only {price: .2f} Dollars!"
+print(txt)
+print(f"{12 * 3}")
