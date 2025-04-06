98e81387d3aa30e62dc0b2a09186f6f443525d97
Reeba <reebarasheedahmed@gmail.com>
2025-04-06 15:41:04 +0500

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,11 @@
+#Set theory (set is a collection of well define words)
+# Sets are unordered collection of data items
+s={2,4,5,6,8,4}
+print(s)
+info={"carle",19,False,5.9,19}
+print(info)
+#reeba={} # ya empty dictionary banay ga empty set nahi
+reeba=set()
+print(type(reeba))
+for value in info:
+    print(value)
