b5080596fe551f10037c101d23ef4f8aea186523
Reeba <reebarasheedahmed@gmail.com>
2025-05-21 10:36:55 +0500

    Add files via upload

---
+++ 17th.py
@@ -0,0 +1,8 @@
+# for loop with else
+for i in range(5):
+    print(i)
+    # break if you put break after loop it execute only one time anf else not execute
+    # continue if you put co tinue after loop it execute all time else execute
+else:
+    print("loop ended")
+# Exception handling in python
